c0f38eb1467293b358d2e40585cea20990bc9461
Snizhana_K <snizhana.knysh@intellecteu.com>
2024-07-09 12:49:42 +0300

    Add app description

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,8 +3,8 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("My app for Users!");
-
+        System.out.println("Super app!");
+        System.out.println("Calculator for base calculations");
         int a = 8;
         int b = 2;
         int c = add(a, b);
